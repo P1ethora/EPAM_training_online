d7865231613d1899e787165110928a995120764c
Rebel-cat <54984087+PAL4D1N@users.noreply.github.com>
2020-10-10 17:35:21 +0300

    Small change

--- Modul_4_Programming_with_classes/m1_simplest_classes_and_objects/Task_01/src/Test1.java
+++ Modul_4_Programming_with_classes/m1_simplest_classes_and_objects/Task_01/src/Test1.java
@@ -2,10 +2,14 @@ class Test1 {
 
     private int a;
     private int b;
+    private int sum;
 
-    void print() {
-        System.out.println("First " + a + " " + "Second " + b);
+    Test1(int a, int b){
+        this.a = a;
+        this.b = b;
     }
+
+    Test1(){}
 
     void setA(int a) {
         this.a = a;
@@ -16,10 +20,17 @@ class Test1 {
     }
 
     void sum() {
-        System.out.println("Sum = " + (a + b));
+        sum = a + b;
+        System.out.println("Sum = " + sum);
     }
 
     void max() {
-        System.out.println("Max = " + (a > b ? a : b));
+        System.out.println("Max = " + (Math.max(a, b)));
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Object {a = " + a + ", b= " + b + "}";
     }
 }
